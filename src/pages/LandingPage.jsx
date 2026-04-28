2291b954db61e6f00991d88a56c0fc5aedb9f997
IAmBaghas <iambaghas@gmail.com>
2026-04-28 19:44:18 +0700

    chore: change landing page import source

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,11 +1,11 @@
 import Navbar from '../components/Navbar'
-import Hero from '../components/Hero'
-import AboutSection from '../components/AboutSection'
-import FeaturesSection from '../components/FeaturesSection'
-import EtalasePreview from '../components/EtalasePreview'
-import MapPreview from '../components/MapPreview'
-import ContributorShowcase from '../components/ContributorShowcase'
-import LoginCTA from '../components/LoginCTA'
+import Hero from '../components/LandingPage/Hero'
+import AboutSection from '../components/LandingPage/AboutSection'
+import FeaturesSection from '../components/LandingPage/FeaturesSection'
+import EtalasePreview from '../components/LandingPage/EtalasePreview'
+import MapPreview from '../components/LandingPage/MapPreview'
+import ContributorShowcase from '../components/LandingPage/ContributorShowcase'
+import LoginCTA from '../components/LandingPage/LoginCTA'
 import Footer from '../components/Footer'
 
 export default function LandingPage() {
