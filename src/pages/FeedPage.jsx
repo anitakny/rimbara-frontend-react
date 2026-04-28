a190dcfb8730af0c28597e4e5918d4d31f5e37e6
IAmBaghas <iambaghas@gmail.com>
2026-04-28 20:50:39 +0700

    feat: beranda placeholder page

---
+++ src/pages/FeedPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+export default function FeedPage() {
+  return (
+    <div>
+        <Navbar />
+        <p>Feed Placeholder Page</p>
+    </div>
+  )
+}
